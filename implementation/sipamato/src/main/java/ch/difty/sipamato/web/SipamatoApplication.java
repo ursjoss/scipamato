455fa77d4f03f18eecfaf181f24c27fb693a00ef
Urs Joss <urs.joss@gmx.ch>
2016-09-06 07:10:09 +0200

    Simplify application class
    * no need to add SpringComponentInjector to ComponentInstantiationListeners (already done in framework)
    * use SpringApplicationBuilder

--- implementation/sipamato/src/main/java/ch/difty/sipamato/web/SipamatoApplication.java
+++ implementation/sipamato/src/main/java/ch/difty/sipamato/web/SipamatoApplication.java
@@ -1,8 +1,7 @@
 package ch.difty.sipamato.web;
 
-import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import com.giffing.wicket.spring.boot.starter.app.WicketBootSecuredWebApplication;
 
@@ -12,13 +11,12 @@ import ch.difty.sipamato.web.pages.home.SipamatoHomePage;
 public class SipamatoApplication extends WicketBootSecuredWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SipamatoApplication.class, args);
+        new SpringApplicationBuilder().sources(SipamatoApplication.class).run(args);
     }
 
     @Override
     protected void init() {
         super.init();
-        getComponentInstantiationListeners().add(new SpringComponentInjector(this));
         mountPackage("/", SipamatoHomePage.class);
     }
 
