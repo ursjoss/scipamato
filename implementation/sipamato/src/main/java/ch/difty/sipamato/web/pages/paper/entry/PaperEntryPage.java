161c84c3d169dfa549794617b4d8c4b19f699a18
Urs Joss <urs.joss@gmx.ch>
2016-10-02 23:36:15 +0200

    Id not enabled

--- implementation/sipamato/src/main/java/ch/difty/sipamato/web/pages/paper/entry/PaperEntryPage.java
+++ implementation/sipamato/src/main/java/ch/difty/sipamato/web/pages/paper/entry/PaperEntryPage.java
@@ -111,7 +111,9 @@ public class PaperEntryPage extends AutoSaveAwarePage<Paper> {
         queueFieldAndLabel(new TextArea<String>(Paper.TITLE), new PropertyValidator<String>());
         queueFieldAndLabel(new TextField<String>(Paper.LOCATION));
 
-        queueFieldAndLabel(new TextField<Integer>(Paper.ID), new PropertyValidator<Integer>());
+        TextField<Integer> id = new TextField<Integer>(Paper.ID);
+        id.setEnabled(false);
+        queueFieldAndLabel(id);
         queueFieldAndLabel(new TextField<Integer>(Paper.PUBL_YEAR), new PropertyValidator<Integer>());
         queueFieldAndLabel(new TextField<Integer>(Paper.PMID));
         queueFieldAndLabel(new TextField<String>(Paper.DOI), new PropertyValidator<String>());
