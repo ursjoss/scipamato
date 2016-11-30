499a035f7a5a1f0ed5fc25e9dee3810b2319bcb1
Urs Joss <urs.joss@gmx.ch>
2016-11-30 22:40:59 +0100

    Refactor and test ComplexPaperFilterProvider

--- implementation/sipamato/src/main/java/ch/difty/sipamato/web/pages/paper/provider/ComplexPaperFilterProvider.java
+++ implementation/sipamato/src/main/java/ch/difty/sipamato/web/pages/paper/provider/ComplexPaperFilterProvider.java
@@ -1,6 +1,7 @@
 package ch.difty.sipamato.web.pages.paper.provider;
 
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.model.IModel;
@@ -8,30 +9,40 @@ import org.apache.wicket.model.Model;
 
 import ch.difty.sipamato.entity.SearchOrder;
 import ch.difty.sipamato.entity.filter.ComplexPaperFilter;
+import ch.difty.sipamato.lib.AssertAs;
 
+/**
+ * Builds up the {@link ComplexPaperFilter} provider based on the {@link SearchOrder} passed into the constructor as model,
+ * exposing the list of {@link ComplexPaperFilter}s contained within.
+ *
+ * @author u.joss
+ */
 public class ComplexPaperFilterProvider extends SortableDataProvider<ComplexPaperFilter, String> {
 
     private static final long serialVersionUID = 1L;
 
-    private IModel<SearchOrder> searchOrderModel;
+    private final List<ComplexPaperFilter> filters;
 
-    public ComplexPaperFilterProvider(IModel<SearchOrder> searchOrderModel) {
-        this.searchOrderModel = searchOrderModel;
+    public ComplexPaperFilterProvider(final IModel<SearchOrder> searchOrderModel) {
+        AssertAs.notNull(searchOrderModel, "searchOrderModel");
+        AssertAs.notNull(searchOrderModel.getObject(), "searchOrder.filters");
+        this.filters = searchOrderModel.getObject().getFilters();
     }
 
     @Override
-    public Iterator<ComplexPaperFilter> iterator(long offset, long size) {
-        return searchOrderModel.getObject().getFilters().iterator();
+    public Iterator<ComplexPaperFilter> iterator(final long offset, final long size) {
+        // TODO currently ignoring offset and size. Might need to implement
+        return filters.iterator();
     }
 
     @Override
     public long size() {
-        return searchOrderModel.getObject().getFilters().size();
+        return filters.size();
     }
 
     @Override
-    public IModel<ComplexPaperFilter> model(ComplexPaperFilter entity) {
-        return new Model<ComplexPaperFilter>(entity);
+    public IModel<ComplexPaperFilter> model(final ComplexPaperFilter filter) {
+        return new Model<ComplexPaperFilter>(filter);
     }
 
 }
