6daecea1880d8cc1b2af9f4256746af85ddd7231
Urs Joss <urs.joss@gmx.ch>
2016-12-09 06:24:17 +0100

    DateTimeService: new method getCurrentDate

--- implementation/sipamato/src/main/java/ch/difty/sipamato/lib/DateTimeService.java
+++ implementation/sipamato/src/main/java/ch/difty/sipamato/lib/DateTimeService.java
@@ -1,11 +1,30 @@
 package ch.difty.sipamato.lib;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+/**
+ * Implementations provide access to date and time.
+ * It's useful to have this as service in order to be mockable.
+ *
+ * @author u.joss
+ */
 public interface DateTimeService {
 
-    public LocalDateTime getCurrentDateTime();
+    /**
+     * @return now as {@link LocalDateTime}
+     */
+    LocalDateTime getCurrentDateTime();
 
-    public Timestamp getCurrentTimestamp();
+    /**
+     * @return today as {@link LocalDate}
+     */
+    LocalDate getCurrentDate();
+
+    /**
+     * @return now as {@link Timestamp}
+     */
+    Timestamp getCurrentTimestamp();
+
 }
