a7efa3fb894700fce68e00b763fe7aa61815dc55
Urs Joss <urs.joss@gmx.ch>
2017-02-08 15:46:04 +0100

    Remove unnecessary parentheses + refactor

--- implementation/sipamato/src/main/java/ch/difty/sipamato/persistance/jooq/paper/searchorder/AuditSearchTermEvaluator.java
+++ implementation/sipamato/src/main/java/ch/difty/sipamato/persistance/jooq/paper/searchorder/AuditSearchTermEvaluator.java
@@ -67,25 +67,26 @@ class AuditSearchTermEvaluator implements SearchTermEvaluator<AuditSearchTerm> {
         if (!(Paper.FLD_CREATED.equals(fieldName) || Paper.FLD_LAST_MOD.equals(fieldName))) {
             checkFields(fieldName, "date", Paper.FLD_CREATED, Paper.FLD_LAST_MOD, token.getType().matchType.name());
         }
-        final Field<Object> field = DSL.field(fieldName);
-        String searchString = token.getDateSqlData();
-        LocalDateTime ldt = LocalDateTime.parse(searchString, DateTimeFormatter.ofPattern(DATE_FORMAT));
-        final Field<Timestamp> value = DSL.val(Timestamp.valueOf(ldt));
+        final LocalDateTime ldt = LocalDateTime.parse(token.getDateSqlData(), DateTimeFormatter.ofPattern(DATE_FORMAT));
+        addToConditions(token, DSL.field(fieldName), DSL.val(Timestamp.valueOf(ldt)), conditions);
+    }
+
+    private void addToConditions(final Token token, final Field<Object> field, final Field<Timestamp> value, final ConditionalSupplier conditions) {
         switch (token.getType().matchType) {
         case GREATER_THAN:
-            conditions.add(() -> (field.greaterThan(value)));
+            conditions.add(() -> field.greaterThan(value));
             break;
         case GREATER_OR_EQUAL:
-            conditions.add(() -> (field.greaterOrEqual(value)));
+            conditions.add(() -> field.greaterOrEqual(value));
             break;
         case EQUALS:
-            conditions.add(() -> (field.equal(value)));
+            conditions.add(() -> field.equal(value));
             break;
         case LESS_OR_EQUAL:
-            conditions.add(() -> (field.lessOrEqual(value)));
+            conditions.add(() -> field.lessOrEqual(value));
             break;
         case LESS_THAN:
-            conditions.add(() -> (field.lessThan(value)));
+            conditions.add(() -> field.lessThan(value));
             break;
         case CONTAINS:
         case NONE:
--- implementation/sipamato/src/main/java/ch/difty/sipamato/persistance/jooq/paper/searchorder/IntegerSearchTermEvaluator.java
+++ implementation/sipamato/src/main/java/ch/difty/sipamato/persistance/jooq/paper/searchorder/IntegerSearchTermEvaluator.java
@@ -1,6 +1,8 @@
 package ch.difty.sipamato.persistance.jooq.paper.searchorder;
 
 import org.jooq.Condition;
+import org.jooq.Field;
+import org.jooq.Param;
 import org.jooq.impl.DSL;
 
 import ch.difty.sipamato.entity.filter.IntegerSearchTerm;
@@ -18,23 +20,26 @@ class IntegerSearchTermEvaluator implements SearchTermEvaluator<IntegerSearchTer
     public Condition evaluate(final IntegerSearchTerm searchTerm) {
         AssertAs.notNull(searchTerm, "searchTerm");
 
+        final Field<Object> field = DSL.field(searchTerm.getFieldName());
+        final Param<Integer> value = DSL.val(searchTerm.getValue());
+
         switch (searchTerm.getType()) {
         case EXACT:
-            return DSL.field(searchTerm.getFieldName()).equal(DSL.val(searchTerm.getValue()));
+            return field.equal(value);
         case LESS_OR_EQUAL:
-            return DSL.field(searchTerm.getFieldName()).le(DSL.val(searchTerm.getValue()));
+            return field.le(value);
         case LESS_THAN:
-            return DSL.field(searchTerm.getFieldName()).lt(DSL.val(searchTerm.getValue()));
+            return field.lt(value);
         case GREATER_OR_EQUAL:
-            return DSL.field(searchTerm.getFieldName()).ge(DSL.val(searchTerm.getValue()));
+            return field.ge(value);
         case GREATER_THAN:
-            return DSL.field(searchTerm.getFieldName()).gt(DSL.val(searchTerm.getValue()));
+            return field.gt(value);
         case RANGE:
-            return DSL.field(searchTerm.getFieldName()).between(DSL.val(searchTerm.getValue()), DSL.val(searchTerm.getValue2()));
+            return field.between(value, DSL.val(searchTerm.getValue2()));
         case MISSING:
-            return DSL.field(searchTerm.getFieldName()).isNull();
+            return field.isNull();
         case PRESENT:
-            return DSL.field(searchTerm.getFieldName()).isNotNull();
+            return field.isNotNull();
         default:
             throw new UnsupportedOperationException("Evaluation of type " + searchTerm.getType() + " is not supported...");
         }
