3842648ae9bc791ae75d9a73968bc75804457fc1
Urs Joss <urs.joss@gmx.ch>
2017-02-08 17:40:48 +0100

    Simplify generics

--- implementation/sipamato/src/main/java/ch/difty/sipamato/persistance/jooq/paper/JooqPaperService.java
+++ implementation/sipamato/src/main/java/ch/difty/sipamato/persistance/jooq/paper/JooqPaperService.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import ch.difty.sipamato.db.tables.records.PaperRecord;
 import ch.difty.sipamato.entity.Paper;
 import ch.difty.sipamato.entity.SearchOrder;
 import ch.difty.sipamato.persistance.jooq.JooqEntityService;
@@ -18,7 +17,7 @@ import ch.difty.sipamato.service.PaperService;
  * @author u.joss
  */
 @Service
-public class JooqPaperService extends JooqEntityService<Long, PaperRecord, Paper, PaperFilter, PaperRecordMapper, PaperRepository> implements PaperService {
+public class JooqPaperService extends JooqEntityService<Long, Paper, PaperFilter, PaperRepository> implements PaperService {
 
     private static final long serialVersionUID = 1L;
 
