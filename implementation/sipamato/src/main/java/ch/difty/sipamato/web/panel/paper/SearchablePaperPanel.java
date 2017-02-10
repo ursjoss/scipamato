9b5cbb37c6f21bdc44208d1a17b20f0e6bdc4d4d
Urs Joss <urs.joss@gmx.ch>
2017-02-10 07:09:54 +0100

    Remove unused methods in SearchablePaperPanel

--- implementation/sipamato/src/main/java/ch/difty/sipamato/web/panel/paper/SearchablePaperPanel.java
+++ implementation/sipamato/src/main/java/ch/difty/sipamato/web/panel/paper/SearchablePaperPanel.java
@@ -17,15 +17,7 @@ public abstract class SearchablePaperPanel extends PaperPanel<SearchCondition> {
 
     private static final long serialVersionUID = 1L;
 
-    public SearchablePaperPanel(String id) {
-        super(id, null, Mode.SEARCH);
-    }
-
     public SearchablePaperPanel(String id, IModel<SearchCondition> model) {
-        super(id, model, Mode.SEARCH);
-    }
-
-    public SearchablePaperPanel(String id, IModel<SearchCondition> model, Mode ignoredMode) {
         super(id, model, Mode.SEARCH);
     }
 
