ba6bd97f5929120b9f6ced280bd5ac062cfd4175
Urs Joss <urs.joss@gmx.ch>
2017-03-15 20:31:35 +0100

    Fix Actuator endpoint security and format

--- implementation/sipamato/src/main/java/ch/difty/sipamato/web/security/WicketWebSecurityConfig.java
+++ implementation/sipamato/src/main/java/ch/difty/sipamato/web/security/WicketWebSecurityConfig.java
@@ -1,7 +1,6 @@
 package ch.difty.sipamato.web.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,13 +10,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import ch.difty.sipamato.auth.Role;
-
 @EnableWebSecurity
 public class WicketWebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Value("${management.context-path}")
-    private String actuatorEndpoints;
 
     @Autowired
     private UserDetailsService userDetailService;
@@ -37,16 +31,11 @@ public class WicketWebSecurityConfig extends WebSecurityConfigurerAdapter {
      // @formatter:off
         http.csrf().disable()
             .authorizeRequests()
-                .antMatchers(actuatorEndpoints()).hasRole(Role.ADMIN.name())
                 .antMatchers("/**").permitAll()
             .and()
                 .logout().permitAll();
         http.headers().frameOptions().disable();
      // @formatter:on
-    }
-
-    private String[] actuatorEndpoints() {
-        return new String[] { actuatorEndpoints };
     }
 
 }
