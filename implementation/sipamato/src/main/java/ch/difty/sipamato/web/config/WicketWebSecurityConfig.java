8159a634b615ddbe54a0586247648cea06d6c782
Urs Joss <urs.joss@gmx.ch>
2017-04-18 17:15:04 +0200

    Move WicketWebSecurityConfig into config package

--- implementation/sipamato/src/main/java/ch/difty/sipamato/web/config/WicketWebSecurityConfig.java
+++ implementation/sipamato/src/main/java/ch/difty/sipamato/web/config/WicketWebSecurityConfig.java
@@ -1,4 +1,4 @@
-package ch.difty.sipamato.web.security;
+package ch.difty.sipamato.web.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
