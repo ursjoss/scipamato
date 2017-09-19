a695a391bb4401e973f2b9cb02b3aed443cf9f04
Urs Joss <urs.joss@gmx.ch>
2017-09-19 07:07:10 +0200

    Lombok on Code

--- implementation/scipamato/scipamato-entity/src/main/java/ch/difty/scipamato/entity/Code.java
+++ implementation/scipamato/scipamato-entity/src/main/java/ch/difty/scipamato/entity/Code.java
@@ -6,7 +6,11 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import ch.difty.scipamato.AssertAs;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
+@Getter
+@EqualsAndHashCode(callSuper = false)
 public class Code extends ScipamatoEntity {
 
     private static final long serialVersionUID = 1L;
@@ -76,77 +80,12 @@ public class Code extends ScipamatoEntity {
         setVersion(version);
     }
 
-    public String getCode() {
-        return code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public boolean isInternal() {
-        return internal;
-    }
-
-    public CodeClass getCodeClass() {
-        return codeClass;
-    }
-
-    public int getSort() {
-        return sort;
-    }
-
     @Override
     public String getDisplayValue() {
         final StringBuilder sb = new StringBuilder();
         sb.append(name);
         sb.append(" (").append(code).append(")");
         return sb.toString();
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + code.hashCode();
-        result = prime * result + codeClass.hashCode();
-        result = prime * result + ((comment == null) ? 0 : comment.hashCode());
-        result = prime * result + (internal ? 1231 : 1237);
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + sort;
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Code other = (Code) obj;
-        if (!code.equals(other.code))
-            return false;
-        if (!codeClass.equals(other.codeClass))
-            return false;
-        if (comment == null) {
-            if (other.comment != null)
-                return false;
-        } else if (!comment.equals(other.comment))
-            return false;
-        if (internal != other.internal)
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        return sort == other.sort;
     }
 
 }
