77693d4ab898262315fa91fe2f898c8a39d52f48
Urs Joss <urs.joss@gmx.ch>
2017-09-19 07:44:54 +0200

    Lombok on BootstrapProperties

--- implementation/scipamato/scipamato-web/src/main/java/ch/difty/scipamato/web/autoconfiguration/BootstrapProperties.java
+++ implementation/scipamato/scipamato-web/src/main/java/ch/difty/scipamato/web/autoconfiguration/BootstrapProperties.java
@@ -4,8 +4,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import de.agilecoders.wicket.core.settings.BootstrapSettings;
 import de.agilecoders.wicket.themes.markup.html.bootswatch.BootswatchTheme;
+import lombok.Getter;
+import lombok.Setter;
 
 @ConfigurationProperties(prefix = BootstrapProperties.PROPERTY_PREFIX)
+@Getter
+@Setter
 public class BootstrapProperties extends BootstrapSettings {
 
     public static final String PROPERTY_PREFIX = "wicket.external.agilcoders.bootstrap";
@@ -14,19 +18,4 @@ public class BootstrapProperties extends BootstrapSettings {
 
     private BootswatchTheme theme = BootswatchTheme.Sandstone;
 
-    public BootswatchTheme getTheme() {
-        return theme;
-    }
-
-    public void setTheme(BootswatchTheme theme) {
-        this.theme = theme;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
 }
