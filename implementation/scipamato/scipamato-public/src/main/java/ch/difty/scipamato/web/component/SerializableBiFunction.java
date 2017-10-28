8f1d8e47890e37339cd99eddea494dd6c42c8600
Urs Joss <urs.joss@gmx.ch>
2017-10-28 09:21:52 +0200

    Fix dependency of scipamato-public on scipamato-wicket and remove obsolete classes from scipamato-public

--- implementation/scipamato/scipamato-public/src/main/java/ch/difty/scipamato/web/component/SerializableBiFunction.java
+++
@@ -1,9 +0,0 @@
-package ch.difty.scipamato.web.component;
-
-import java.io.Serializable;
-import java.util.function.BiFunction;
-
-@FunctionalInterface
-public interface SerializableBiFunction<T, U, R> extends BiFunction<T, U, R>, Serializable {
-
-}
