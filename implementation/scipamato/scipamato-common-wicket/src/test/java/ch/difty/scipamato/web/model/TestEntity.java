ca4b54c0ca7ec92279595543e3ddf935b1a941db
Urs Joss <urs.joss@gmx.ch>
2017-11-26 23:38:14 +0100

    Remove some duplication

---
+++ implementation/scipamato/scipamato-common-wicket/src/test/java/ch/difty/scipamato/web/model/TestEntity.java
@@ -0,0 +1,8 @@
+package ch.difty.scipamato.web.model;
+
+import ch.difty.scipamato.entity.ScipamatoEntity;
+
+class TestEntity extends ScipamatoEntity {
+    private static final long serialVersionUID = 1L;
+
+}
