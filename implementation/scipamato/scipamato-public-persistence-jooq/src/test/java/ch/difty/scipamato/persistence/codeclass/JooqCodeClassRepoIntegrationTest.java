64193498756171c8f750bc24d30774b6a42ecc69
Urs Joss <urs.joss@gmx.ch>
2017-11-26 16:06:50 +0100

    Public 07/24: Code and CodeClass Repo and Service

---
+++ implementation/scipamato/scipamato-public-persistence-jooq/src/test/java/ch/difty/scipamato/persistence/codeclass/JooqCodeClassRepoIntegrationTest.java
@@ -0,0 +1,30 @@
+package ch.difty.scipamato.persistence.codeclass;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ch.difty.scipamato.persistence.JooqTransactionalIntegrationTest;
+
+public class JooqCodeClassRepoIntegrationTest extends JooqTransactionalIntegrationTest {
+
+    @Autowired
+    private JooqCodeClassRepo repo;
+
+    @Test
+    public void finding_inEnglish_returnsLocalizedCodeClasses() {
+        assertThat(repo.find("en")).extracting("name").containsExactly("Exposure Agent", "Region", "Study Population", "Health Outcome", "Study Design", "Species", "Duration of Exposure", "Setting");
+    }
+
+    @Test
+    public void finding_inFrench_returnsLocalizedCodeClasses() {
+        assertThat(repo.find("fr")).extracting("name").containsExactly("Polluant nocif", "Région", "Echantillons des personnes", "Effets physiologigue ou nocifs", "Typ d'étude", "Espèces investigées",
+                "Durée de l'exposition", "Site d'exposition");
+    }
+
+    @Test
+    public void finding_inGerman_returnsLocalizedCodeClasses() {
+        assertThat(repo.find("de")).extracting("name").containsExactly("Schadstoffe", "Region", "Kollektiv", "Zielgrössen", "Studientyp", "Spezies", "Zeitdauer", "Umgebung");
+    }
+}
