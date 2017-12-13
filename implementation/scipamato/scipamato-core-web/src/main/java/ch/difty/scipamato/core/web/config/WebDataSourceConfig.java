b3a7e9f6454fd0d547785f8a2783148910d39460
Urs Joss <urs.joss@gmx.ch>
2017-12-13 20:53:08 +0100

    Remove obsolete ipmort

--- implementation/scipamato/scipamato-core-web/src/main/java/ch/difty/scipamato/core/web/config/WebDataSourceConfig.java
+++ implementation/scipamato/scipamato-core-web/src/main/java/ch/difty/scipamato/core/web/config/WebDataSourceConfig.java
@@ -8,7 +8,6 @@ import org.jooq.impl.DSL;
 import org.jooq.impl.DefaultConfiguration;
 import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
