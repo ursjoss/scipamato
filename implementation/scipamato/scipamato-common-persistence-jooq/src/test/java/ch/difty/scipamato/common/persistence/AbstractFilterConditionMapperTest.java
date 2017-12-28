c7a441af94ee03abb0769b07b322803c488a87a6
Urs Joss <urs.joss@gmx.ch>
2017-12-28 17:06:32 +0100

    AbstractFilterConditionMapperTest

---
+++ implementation/scipamato/scipamato-common-persistence-jooq/src/test/java/ch/difty/scipamato/common/persistence/AbstractFilterConditionMapperTest.java
@@ -0,0 +1,40 @@
+package ch.difty.scipamato.common.persistence;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.jooq.Condition;
+import org.junit.Test;
+
+import ch.difty.scipamato.common.entity.filter.ScipamatoFilter;
+
+public class AbstractFilterConditionMapperTest {
+
+    private boolean filterCalled = false;
+
+    private static class DummyFilter extends ScipamatoFilter {
+        private static final long serialVersionUID = 1L;
+    }
+
+    private final AbstractFilterConditionMapper<DummyFilter> mapper = new AbstractFilterConditionMapper<DummyFilter>() {
+        @Override
+        protected void map(DummyFilter filter, List<Condition> conditions) {
+            filterCalled = true;
+        }
+    };
+
+    @Test
+    public void mappingWithNullFilter_doesNotMap() {
+        assertThat(mapper.map(null)
+            .toString()).isEqualTo("1 = 1");
+        assertThat(filterCalled).isFalse();
+    }
+
+    @Test
+    public void mappingWithNonNullFilter_doesMap() {
+        mapper.map(new DummyFilter());
+        assertThat(filterCalled).isTrue();
+    }
+
+}
