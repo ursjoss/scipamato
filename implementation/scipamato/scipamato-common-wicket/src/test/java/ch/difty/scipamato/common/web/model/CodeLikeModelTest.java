4e3047fe327ee844ed6d4c3a72cbde7352131443
Urs Joss <urs.joss@gmx.ch>
2018-01-10 14:31:33 +0100

    CodeLikeModelTest/CodeClassLikeModelTest + slight refactoring of the classes to make them testable

---
+++ implementation/scipamato/scipamato-common-wicket/src/test/java/ch/difty/scipamato/common/web/model/CodeLikeModelTest.java
@@ -0,0 +1,68 @@
+package ch.difty.scipamato.common.web.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import ch.difty.scipamato.common.entity.CodeClassId;
+import ch.difty.scipamato.common.entity.CodeLike;
+import ch.difty.scipamato.common.persistence.CodeLikeService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CodeLikeModelTest {
+
+    private static final String      LANG_CODE = "en";
+    private static final CodeClassId CC_ID     = CodeClassId.CC1;
+
+    private CodeLikeModel<CodeLike, CodeLikeService<CodeLike>> model;
+
+    private final List<CodeLike> ccls = new ArrayList<>();
+
+    @Mock
+    private CodeLike cclMock;
+
+    @Mock
+    private CodeLikeService<CodeLike> serviceMock;
+
+    @Before
+    public void setUp() {
+        model = new CodeLikeModel<CodeLike, CodeLikeService<CodeLike>>(CC_ID, LANG_CODE, serviceMock) {
+            private static final long serialVersionUID = 1L;
+
+            protected void injectThis() {
+                // no-op
+            }
+        };
+
+        ccls.add(cclMock);
+        ccls.add(cclMock);
+
+        when(serviceMock.findCodesOfClass(CC_ID, LANG_CODE)).thenReturn(ccls);
+    }
+
+    @Test
+    public void canGetCodeClass() {
+        assertThat(model.getCodeClassId()).isEqualTo(CC_ID);
+    }
+
+    @Test
+    public void canGetLanguageCode() {
+        assertThat(model.getLanguageCode()).isEqualTo(LANG_CODE);
+    }
+
+    @Test
+    public void modelObject_gotCodeClasssesFromService() {
+        assertThat(model.getObject()).containsExactly(cclMock, cclMock);
+        verify(serviceMock).findCodesOfClass(CC_ID, LANG_CODE);
+    }
+
+}
