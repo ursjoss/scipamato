10a1c9c10843a83a683d560f1007ac03f39536ad
Urs Joss <urs.joss@gmx.ch>
2018-01-14 10:06:24 +0100

    Note to self: *ALWAYS* run the test before pushing :-(

--- implementation/scipamato/scipamato-common-wicket/src/main/java/ch/difty/scipamato/common/web/config/UndertowConfig.java
+++ implementation/scipamato/scipamato-common-wicket/src/main/java/ch/difty/scipamato/common/web/config/UndertowConfig.java
@@ -12,8 +12,10 @@ import io.undertow.servlet.api.SecurityConstraint;
 import io.undertow.servlet.api.SecurityInfo;
 import io.undertow.servlet.api.TransportGuaranteeType;
 import io.undertow.servlet.api.WebResourceCollection;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
+@Slf4j
 public class UndertowConfig {
 
     private final ServerProperties      serverProperties;
@@ -27,21 +29,26 @@ public class UndertowConfig {
     /**
      * Bean configuring the embedded undertow server to forward an unencrypted
      * connection from a specific port {@literal scipamato.redirect-from-port} to
-     * the port used for https {@literal server.port}. The bean is only instantiated
-     * if the property {@literal scipamato.redirect-from-port} is defined.
+     * the confidential port used for https {@literal server.port}. The bean is only
+     * instantiated if the property {@literal scipamato.redirect-from-port} is
+     * defined.
      */
     @Bean
     @ConditionalOnProperty(name = "scipamato.redirect-from-port", relaxedNames = true)
     public EmbeddedServletContainerFactory undertow() {
+        final Integer redirectFromPort = scipamatoProperties.getRedirectFromPort();
+        final Integer confidentialPort = serverProperties.getPort();
+        log.info("Adding http listener on port {} redirecting to confidential port {} for https.", redirectFromPort,
+            confidentialPort);
         final UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
-        factory.addBuilderCustomizers(
-            builder -> builder.addHttpListener(scipamatoProperties.getRedirectFromPort(), "0.0.0.0"));
+        factory.addBuilderCustomizers(builder -> builder.addHttpListener(redirectFromPort, "0.0.0.0"));
         factory.addDeploymentInfoCustomizers(deploymentInfo -> deploymentInfo
             .addSecurityConstraint(
                 new SecurityConstraint().addWebResourceCollection(new WebResourceCollection().addUrlPattern("/*"))
                     .setTransportGuaranteeType(TransportGuaranteeType.CONFIDENTIAL)
                     .setEmptyRoleSemantic(SecurityInfo.EmptyRoleSemantic.PERMIT))
-            .setConfidentialPortManager(exchange -> serverProperties.getPort()));
+            .setConfidentialPortManager(httpExchange -> confidentialPort));
+        log.debug("UndertowEmbeddedServletContainerFactory configured.");
         return factory;
     }
 
