a80a7f825b2a5f76eb4200956dbe3d1998af1670
Urs Joss <urs.joss@gmx.ch>
2018-02-26 07:02:45 +0100

    Move BootstrapConfig and BootstrapProperties from common into web projects

---
+++ implementation/scipamato/scipamato-common-wicket/src/test/java/ch/difty/scipamato/common/web/autoconfiguration/BootstrapConfig.java
@@ -0,0 +1,25 @@
+package ch.difty.scipamato.common.web.autoconfiguration;
+
+import org.apache.wicket.protocol.http.WebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+
+import com.giffing.wicket.spring.boot.context.extensions.ApplicationInitExtension;
+import com.giffing.wicket.spring.boot.context.extensions.WicketApplicationInitConfiguration;
+
+import de.agilecoders.wicket.core.Bootstrap;
+
+@ApplicationInitExtension
+@ConditionalOnProperty(prefix = "wicket.external.agilcoders.bootstrap", value = "enabled", matchIfMissing = true)
+@ConditionalOnClass(BootstrapConfig.class)
+public class BootstrapConfig implements WicketApplicationInitConfiguration {
+
+    public BootstrapConfig() {
+    }
+
+    @Override
+    public void init(WebApplication webApplication) {
+        Bootstrap.install(webApplication);
+    }
+
+}
---
+++ implementation/scipamato/scipamato-public-web/src/main/java/ch/difty/scipamato/publ/web/autoconfiguration/BootstrapConfig.java
@@ -0,0 +1,38 @@
+package ch.difty.scipamato.publ.web.autoconfiguration;
+
+import org.apache.wicket.protocol.http.WebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.giffing.wicket.spring.boot.context.extensions.ApplicationInitExtension;
+import com.giffing.wicket.spring.boot.context.extensions.WicketApplicationInitConfiguration;
+
+import de.agilecoders.wicket.core.Bootstrap;
+import de.agilecoders.wicket.core.settings.ThemeProvider;
+import de.agilecoders.wicket.less.BootstrapLess;
+import de.agilecoders.wicket.themes.markup.html.bootswatch.BootswatchThemeProvider;
+
+@ApplicationInitExtension
+@ConditionalOnProperty(prefix = BootstrapProperties.PROPERTY_PREFIX, value = "enabled", matchIfMissing = true)
+@ConditionalOnClass(BootstrapConfig.class)
+@EnableConfigurationProperties({ BootstrapProperties.class })
+public class BootstrapConfig implements WicketApplicationInitConfiguration {
+
+    private final BootstrapProperties prop;
+
+    public BootstrapConfig(BootstrapProperties prop) {
+        this.prop = prop;
+    }
+
+    @Override
+    public void init(WebApplication webApplication) {
+        final ThemeProvider themeProvider = new BootswatchThemeProvider(prop.getTheme());
+        prop.setThemeProvider(themeProvider);
+
+        Bootstrap.install(webApplication, prop);
+
+        BootstrapLess.install(webApplication);
+    }
+
+}
