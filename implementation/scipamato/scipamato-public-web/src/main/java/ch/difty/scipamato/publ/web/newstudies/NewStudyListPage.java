92bb9cdcc96aad4ba1f8e799c1970d780cb195c9
Urs Joss <urs.joss@gmx.ch>
2018-03-13 06:18:06 +0100

    Clean up NewStudyListPage and Test

--- implementation/scipamato/scipamato-public-web/src/main/java/ch/difty/scipamato/publ/web/newstudies/NewStudyListPage.java
+++ implementation/scipamato/scipamato-public-web/src/main/java/ch/difty/scipamato/publ/web/newstudies/NewStudyListPage.java
@@ -18,6 +18,18 @@ import ch.difty.scipamato.publ.web.PageParameters;
 import ch.difty.scipamato.publ.web.common.BasePage;
 import ch.difty.scipamato.publ.web.paper.browse.PublicPaperDetailPage;
 
+/**
+ * The page lists 'new studies', i.e. studies that were collected and flagged by
+ * the SciPaMaTo-team as eligible for this page. By default, the newest
+ * collection of new studies is presented.
+ *
+ * With the use of page-parameters, an older collection of new studies can be
+ * selected instead (TODO).
+ *
+ * The page is typically shown in an iframe of a CMS.
+ *
+ * @author Urs Joss
+ */
 @MountPath("new-studies")
 public class NewStudyListPage extends BasePage<Void> {
 
@@ -26,7 +38,7 @@ public class NewStudyListPage extends BasePage<Void> {
     @SpringBean
     private NewStudyTopicService newStudyTopicService;
 
-    public NewStudyListPage(org.apache.wicket.request.mapper.parameter.PageParameters parameters) {
+    public NewStudyListPage(final org.apache.wicket.request.mapper.parameter.PageParameters parameters) {
         super(parameters);
     }
 
@@ -37,34 +49,37 @@ public class NewStudyListPage extends BasePage<Void> {
         queue(newLabel("h1Title"));
 
         queue(newLabel("introParagraph"));
-        queue(newLink("dbLink", "http://ludok.swisstph.ch"));
+        queue(newLink("dbLink", getProperties().getCmsUrlSearchPage()));
 
         queue(newNewStudyCollection("topics"));
     }
 
-    private Label newLabel(String id) {
+    private Label newLabel(final String id) {
         return new Label(id, new StringResourceModel(id + LABEL_RESOURCE_TAG, this, null).getString());
     }
 
-    private ExternalLink newLink(String id, String href) {
+    private ExternalLink newLink(final String id, final String href) {
         return new ExternalLink(id, href, new StringResourceModel(id + LABEL_RESOURCE_TAG, this, null).getString());
     }
 
-    private ListView<NewStudyTopic> newNewStudyCollection(String id) {
+    private ListView<NewStudyTopic> newNewStudyCollection(final String id) {
         return new ListView<NewStudyTopic>(id, newStudyTopicService.findMostRecentNewStudyTopics()) {
             private static final long serialVersionUID = 1L;
 
             @Override
             protected void populateItem(ListItem<NewStudyTopic> topic) {
-                topic.add(new Label("topicTitle", new PropertyModel<String>(topic.getModel(), "title")));
+                topic.add(new Label("topicTitle", new PropertyModel<String>(topic.getModel(),
+                        NewStudyTopic.NewStudyTopicFields.TITLE.getName())));
                 topic.add(new ListView<NewStudy>("topicStudies", topic.getModelObject()
                     .getStudies()) {
                     private static final long serialVersionUID = 1L;
 
                     @Override
                     protected void populateItem(ListItem<NewStudy> study) {
-                        study.add(new Label("headline", new PropertyModel<String>(study.getModel(), "headline")));
-                        study.add(new Label("description", new PropertyModel<String>(study.getModel(), "description")));
+                        study.add(new Label("headline", new PropertyModel<String>(study.getModel(),
+                                NewStudy.NewStudyFields.HEADLINE.getName())));
+                        study.add(new Label("description", new PropertyModel<String>(study.getModel(),
+                                NewStudy.NewStudyFields.DESCRIPTION.getName())));
                         study.add(newLinkToStudy("reference", study));
                     }
                 });
@@ -72,12 +87,23 @@ public class NewStudyListPage extends BasePage<Void> {
         };
     }
 
-    private Link<NewStudy> newLinkToStudy(String id, ListItem<NewStudy> study) {
+    /**
+     * Link pointing to the study detail page with the current study
+     *
+     * @param id
+     *            the id on the html page. Also expects a label with the same id +
+     *            tag 'Label'
+     * @param study
+     *            the current study as ListItem
+     * @return the link
+     */
+    private Link<NewStudy> newLinkToStudy(final String id, final ListItem<NewStudy> study) {
         org.apache.wicket.request.mapper.parameter.PageParameters pp = new org.apache.wicket.request.mapper.parameter.PageParameters();
         pp.set(PageParameters.NUMBER.getName(), study.getModelObject()
             .getNumber());
         Link<NewStudy> link = new BookmarkablePageLink<>(id, PublicPaperDetailPage.class, pp);
-        link.add(new Label("referenceLabel", new PropertyModel<String>(study.getModel(), "reference")));
+        link.add(new Label(id + "Label",
+                new PropertyModel<String>(study.getModel(), NewStudy.NewStudyFields.REFERENCE.getName())));
         return link;
     }
 
