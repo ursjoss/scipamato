02b3ef1a80eca24003781c257f05da0d46fccf58
Urs Joss <urs.joss@gmx.ch>
2018-04-20 09:18:43 +0200

    Refactor and test the UtilConfiguration classes

--- implementation/scipamato/scipamato-core-web/src/main/java/ch/difty/scipamato/common/UtilConfiguration.java
+++ implementation/scipamato/scipamato-core-web/src/main/java/ch/difty/scipamato/common/UtilConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class UtilTestConfiguration {
+public class UtilConfiguration {
 
     @Bean
     public DateTimeService dateTimeService() {
