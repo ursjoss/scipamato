e0faf183d0ad74fa85a0e28a348167797850d32b
Urs Joss <urs.joss@gmx.ch>
2019-02-01 21:47:23 +0100

    SciPaMatoPublicApplicationIntegrationTest

---
+++ implementation/scipamato/scipamato-public-web/src/test/java/ch/difty/scipamato/publ/SciPaMaToPublicApplicationIntegrationTest.java
@@ -0,0 +1,15 @@
+package ch.difty.scipamato.publ;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SciPaMaToPublicApplicationIntegrationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
