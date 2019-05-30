f08af37385519f5e4cfe62338a00ed362cfe8bd6
Urs Joss <u.joss@calltrade.ch>
2019-05-30 11:42:33 +0200

    Bug/84: Add some integration tests capturing the id issue

--- implementation/scipamato/scipamato-core-persistence-jooq/src/test/java/ch/difty/scipamato/core/persistence/paper/slim/JooqPaperSlimBySearchOrderRepoIntegrationTest.java
+++ implementation/scipamato/scipamato-core-persistence-jooq/src/test/java/ch/difty/scipamato/core/persistence/paper/slim/JooqPaperSlimBySearchOrderRepoIntegrationTest.java
@@ -61,4 +61,56 @@ class JooqPaperSlimBySearchOrderRepoIntegrationTest extends JooqBaseIntegrationT
         assertThat(count).isGreaterThanOrEqualTo(0);
     }
 
+    @Test
+    void findingPaged_withSearchConditionUsingIdRange() {
+        SearchOrder so = new SearchOrder();
+        SearchCondition sc = new SearchCondition();
+        sc.setId("10-15");
+        so.add(sc);
+
+        PaginationContext pc = new PaginationRequest(0, 10);
+        List<PaperSlim> papers = repo.findPageBySearchOrder(so, pc);
+
+        assertThat(papers).hasSize(6);
+    }
+
+    @Test
+    void findingPaged_withSearchConditionUsingIdLessThan() {
+        SearchOrder so = new SearchOrder();
+        SearchCondition sc = new SearchCondition();
+        sc.setId("<11");
+        so.add(sc);
+
+        PaginationContext pc = new PaginationRequest(0, 10);
+        List<PaperSlim> papers = repo.findPageBySearchOrder(so, pc);
+
+        assertThat(papers).hasSize(5);
+    }
+
+    @Test
+    void findingPaged_withSearchConditionUsingNumberGreaterThan() {
+        SearchOrder so = new SearchOrder();
+        SearchCondition sc = new SearchCondition();
+        sc.setNumber("23");
+        so.add(sc);
+
+        PaginationContext pc = new PaginationRequest(0, 10);
+        List<PaperSlim> papers = repo.findPageBySearchOrder(so, pc);
+
+        assertThat(papers).hasSize(1);
+    }
+
+    @Test
+    void findingPaged_withSearchConditionUsingPublicationYearRange() {
+        SearchOrder so = new SearchOrder();
+        SearchCondition sc = new SearchCondition();
+        sc.setPublicationYear("<2015");
+        so.add(sc);
+
+        PaginationContext pc = new PaginationRequest(0, 10);
+        List<PaperSlim> papers = repo.findPageBySearchOrder(so, pc);
+
+        assertThat(papers).hasSize(6);
+    }
+
 }
