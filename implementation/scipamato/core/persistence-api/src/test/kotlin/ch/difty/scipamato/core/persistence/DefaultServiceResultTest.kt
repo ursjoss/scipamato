c5ed6cb42bcd17a732e6f6c30785d84403bc5931
Urs Joss <urs.joss@gmx.ch>
2019-09-14 15:13:27 +0200

    core-persistence-api: convert tests to kotlin

---
+++ implementation/scipamato/core/persistence-api/src/test/kotlin/ch/difty/scipamato/core/persistence/DefaultServiceResultTest.kt
@@ -0,0 +1,67 @@
+package ch.difty.scipamato.core.persistence
+
+import org.assertj.core.api.Assertions.assertThat
+
+import org.junit.jupiter.api.Test
+
+internal class DefaultServiceResultTest {
+
+    private val sr = DefaultServiceResult()
+
+    @Test
+    fun defaultServiceResult_hasNoMessages() {
+        assertThat(sr.infoMessages).isEmpty()
+        assertThat(sr.warnMessages).isEmpty()
+        assertThat(sr.errorMessages).isEmpty()
+    }
+
+    @Test
+    fun addingInfoMessages() {
+        sr.addInfoMessage("foo")
+        assertThat(sr.infoMessages).containsOnly("foo")
+        assertThat(sr.warnMessages).isEmpty()
+        assertThat(sr.errorMessages).isEmpty()
+
+        sr.addInfoMessage("bar")
+        assertThat(sr.infoMessages).containsOnly("foo", "bar")
+        assertThat(sr.warnMessages).isEmpty()
+        assertThat(sr.errorMessages).isEmpty()
+    }
+
+    @Test
+    fun addingWarnMessages() {
+        sr.addWarnMessage("foo")
+        assertThat(sr.warnMessages).containsOnly("foo")
+        assertThat(sr.infoMessages).isEmpty()
+        assertThat(sr.errorMessages).isEmpty()
+
+        sr.addWarnMessage("bar")
+        assertThat(sr.warnMessages).containsOnly("foo", "bar")
+        assertThat(sr.infoMessages).isEmpty()
+        assertThat(sr.errorMessages).isEmpty()
+    }
+
+    @Test
+    fun addingErrorMessages() {
+        sr.addErrorMessage("foo")
+        assertThat(sr.errorMessages).containsOnly("foo")
+        assertThat(sr.infoMessages).isEmpty()
+        assertThat(sr.warnMessages).isEmpty()
+
+        sr.addErrorMessage("bar")
+        assertThat(sr.errorMessages).containsOnly("foo", "bar")
+        assertThat(sr.infoMessages).isEmpty()
+        assertThat(sr.warnMessages).isEmpty()
+    }
+
+    @Test
+    fun ignoringNullMessages() {
+        sr.addInfoMessage(null)
+        sr.addWarnMessage(null)
+        sr.addErrorMessage(null)
+
+        assertThat(sr.infoMessages).isEmpty()
+        assertThat(sr.warnMessages).isEmpty()
+        assertThat(sr.errorMessages).isEmpty()
+    }
+}
