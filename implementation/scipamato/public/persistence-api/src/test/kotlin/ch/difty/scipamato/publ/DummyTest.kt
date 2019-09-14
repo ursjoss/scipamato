25aeb97077d6333b94ca263496f458e578c98561
Urs Joss <urs.joss@gmx.ch>
2019-09-14 14:12:04 +0200

    public-persistence-api: convert dummy test to kotlin

---
+++ implementation/scipamato/public/persistence-api/src/test/kotlin/ch/difty/scipamato/publ/DummyTest.kt
@@ -0,0 +1,16 @@
+package ch.difty.scipamato.publ
+
+import org.assertj.core.api.Assertions.assertThat
+
+import org.junit.jupiter.api.Test
+
+/**
+ * Dummy test to trigger jacoco to create the test.exec file for sonar.
+ */
+internal class DummyTest {
+
+    @Test
+    fun dummyTest() {
+        assertThat(true).isTrue()
+    }
+}
