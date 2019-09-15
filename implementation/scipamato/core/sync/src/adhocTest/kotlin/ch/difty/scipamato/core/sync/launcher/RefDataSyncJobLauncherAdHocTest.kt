c78faa3f1b36c34d4473632007bb0d0b8319fa76
Urs Joss <urs.joss@gmx.ch>
2019-09-15 21:25:23 +0200

    core-sync: convert adhoc test to kotlin

---
+++ implementation/scipamato/core/sync/src/adhocTest/kotlin/ch/difty/scipamato/core/sync/launcher/RefDataSyncJobLauncherAdHocTest.kt
@@ -0,0 +1,21 @@
+package ch.difty.scipamato.core.sync.launcher
+
+import org.assertj.core.api.Assertions.assertThat
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal open class RefDataSyncJobLauncherAdHocTest {
+
+    @Autowired
+    private lateinit var launcher: RefDataSyncJobLauncher
+
+    @Test
+    fun run() {
+        val result = launcher.launch()
+        result.messages.forEach { println(it) }
+        assertThat(result.isSuccessful).isTrue()
+    }
+}
