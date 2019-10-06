09723db16c044fdec0193185fbd60eff39b3d062
Urs Joss <urs.joss@gmx.ch>
2019-10-06 08:55:28 +0200

    Bump test-containers from 1.12.1 to 1.12.2

--- implementation/scipamato/buildSrc/src/main/kotlin/Lib.kt
+++ implementation/scipamato/buildSrc/src/main/kotlin/Lib.kt
@@ -42,7 +42,7 @@ object Lib {
 
     private const val junit5Version = "5.5.2"
 
-    private const val testcontainersVersion = "1.12.1"
+    private const val testcontainersVersion = "1.12.2"
 
     @Suppress("MemberVisibilityCanBePrivate")
     const val mockitoVersion = "3.0.6"
