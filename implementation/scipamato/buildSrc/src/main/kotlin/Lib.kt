3cf21bd5ddf6ba6bdadceed34b24f30a9fbae5cc
Urs Joss <urs.joss@gmx.ch>
2019-10-06 08:40:01 +0200

    Bump spring-cloud-starter from 2.1.2 to 2.1.3

--- implementation/scipamato/buildSrc/src/main/kotlin/Lib.kt
+++ implementation/scipamato/buildSrc/src/main/kotlin/Lib.kt
@@ -13,7 +13,7 @@ object Lib {
     private const val springBootVersion = "2.1.9.RELEASE"
     private const val springBootAdminVersion = "2.1.6"
     const val springCloudVersion = "Greenwich.SR3"
-    private const val springCloudStarterVersion = "2.1.2.RELEASE"
+    private const val springCloudStarterVersion = "2.1.3.RELEASE"
     private const val springDependencyManagementVersion = "1.0.8.RELEASE"
 
     private const val wicketSpringBootStarterVersion = "2.1.7"
