23e3779d2b2d3f390011253de2c5a597a43540b6
Urs Joss <urs.joss@gmx.ch>
2019-10-08 22:32:30 +0200

    Resolve some more of the sonar reported issues

--- implementation/scipamato/common/utils/src/main/kotlin/ch/difty/scipamato/common/paper/AbstractShortFieldConcatenator.kt
+++ implementation/scipamato/common/utils/src/main/kotlin/ch/difty/scipamato/common/paper/AbstractShortFieldConcatenator.kt
@@ -8,38 +8,105 @@ package ch.difty.scipamato.common.paper
  * pre-pending an English static label to each value. The other one with dynamic labels that can be localized according
  * to the users language preference.
  */
+@SuppressWarnings("LongParameterList")
 abstract class AbstractShortFieldConcatenator protected constructor(private val withNewLine: Boolean) {
 
-    fun methodsFrom(method: String?, methodStudyDesign: String?, methodOutcome: String?, populationPlace: String?,
-                    exposurePollutant: String?, exposureAssessment: String?, methodStatistics: String?, methodConfounders: String?
-    ) = methodsFrom(method, Tuple("Study Design", methodStudyDesign), Tuple("Outcome", methodOutcome),
-            Tuple("Place", populationPlace), Tuple("Pollutant", exposurePollutant),
-            Tuple("Exposure Assessment", exposureAssessment), Tuple("Statistical Method", methodStatistics),
+    fun methodsFrom(
+            method: String?,
+            methodStudyDesign: String?,
+            methodOutcome: String?,
+            populationPlace: String?,
+            exposurePollutant: String?,
+            exposureAssessment: String?,
+            methodStatistics: String?,
+            methodConfounders: String?
+    ) = methodsFrom(
+            method,
+            Tuple("Study Design", methodStudyDesign),
+            Tuple("Outcome", methodOutcome),
+            Tuple("Place", populationPlace),
+            Tuple("Pollutant", exposurePollutant),
+            Tuple("Exposure Assessment", exposureAssessment),
+            Tuple("Statistical Method", methodStatistics),
             Tuple("Confounders", methodConfounders))
 
-    fun methodsFrom(method: String?, methodStudyDesign: Tuple?, methodOutcome: Tuple?, populationPlace: Tuple?, exposurePollutant: Tuple?,
-                    exposureAssessment: Tuple?, methodStatistics: Tuple?, methodConfounders: Tuple?
-    ) = determineAppropriate(Tuple(null, method), methodStudyDesign, methodOutcome, populationPlace, exposurePollutant, exposureAssessment,
-            methodStatistics, methodConfounders)
+    fun methodsFrom(
+            method: String?,
+            methodStudyDesign: Tuple?,
+            methodOutcome: Tuple?,
+            populationPlace: Tuple?,
+            exposurePollutant: Tuple?,
+            exposureAssessment: Tuple?,
+            methodStatistics: Tuple?,
+            methodConfounders: Tuple?
+    ) = determineAppropriate(
+            Tuple(null, method),
+            methodStudyDesign,
+            methodOutcome,
+            populationPlace,
+            exposurePollutant,
+            exposureAssessment,
+            methodStatistics,
+            methodConfounders
+    )
 
-    fun populationFrom(population: String?, populationPlace: String?, populationParticipants: String?, populationDuration: String?) =
-            populationFrom(population, Tuple("Place", populationPlace), Tuple("Participants", populationParticipants),
-                    Tuple("Study Duration", populationDuration))
+    fun populationFrom(
+            population: String?,
+            populationPlace: String?,
+            populationParticipants: String?,
+            populationDuration: String?
+    ) = populationFrom(
+            population,
+            Tuple("Place", populationPlace),
+            Tuple("Participants", populationParticipants),
+            Tuple("Study Duration", populationDuration)
+    )
 
-    fun populationFrom(population: String?, populationPlace: Tuple?, populationParticipants: Tuple?, populationDuration: Tuple?) =
-            determineAppropriate(Tuple(null, population), populationPlace, populationParticipants, populationDuration)
+    fun populationFrom(
+            population: String?,
+            populationPlace: Tuple?,
+            populationParticipants: Tuple?,
+            populationDuration: Tuple?
+    ) = determineAppropriate(
+            Tuple(null, population),
+            populationPlace,
+            populationParticipants,
+            populationDuration
+    )
 
-    fun resultFrom(result: String?, resultMeasuredOutcome: String?, resultExposureRange: String?, resultEffectEstimate: String?, conclusion: String?) =
-            resultFrom(result, Tuple("Measured Outcome", resultMeasuredOutcome), Tuple("Exposure (Range)", resultExposureRange),
-                    Tuple("Effect Estimate", resultEffectEstimate), Tuple("Conclusion", conclusion))
+    fun resultFrom(
+            result: String?,
+            resultMeasuredOutcome: String?,
+            resultExposureRange: String?,
+            resultEffectEstimate: String?,
+            conclusion: String?
+    ) = resultFrom(
+            result,
+            Tuple("Measured Outcome", resultMeasuredOutcome),
+            Tuple("Exposure (Range)", resultExposureRange),
+            Tuple("Effect Estimate", resultEffectEstimate),
+            Tuple("Conclusion", conclusion)
+    )
 
-    fun resultFrom(result: String?, resultMeasuredOutcome: Tuple?, resultExposureRange: Tuple?, resultEffectEstimate: Tuple?, conclusion: Tuple?) =
-            determineAppropriate(Tuple(null, result), resultMeasuredOutcome, resultExposureRange, resultEffectEstimate, conclusion)
+    fun resultFrom(
+            result: String?,
+            resultMeasuredOutcome: Tuple?,
+            resultExposureRange: Tuple?,
+            resultEffectEstimate: Tuple?,
+            conclusion: Tuple?
+    ) = determineAppropriate(
+            Tuple(null, result),
+            resultMeasuredOutcome,
+            resultExposureRange,
+            resultEffectEstimate,
+            conclusion
+    )
 
     /**
      * Use the [mainField] (with precedence) if not blank. Concatenate all other fields passed in via [shortFields]
      * (separated by ' - ') otherwise. Working with tuples of a label (English only) and the value.
      */
+    @Suppress("SpreadOperator")
     private fun determineAppropriate(mainField: Tuple, vararg shortFields: Tuple?): String =
             mainField.content ?: if (shortFields.isNotEmpty()) concat(*shortFields) else ""
 
