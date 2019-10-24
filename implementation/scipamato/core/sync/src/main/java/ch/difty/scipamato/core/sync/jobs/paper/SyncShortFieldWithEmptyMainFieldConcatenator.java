30199634febc4bd22784e5cc9b114615fc857ec5
Urs Joss <urs.joss@gmx.ch>
2019-10-24 08:48:28 +0200

    Remove unused field in SyncShortFieldWithEmptyMainFieldConcatenator

--- implementation/scipamato/core/sync/src/main/java/ch/difty/scipamato/core/sync/jobs/paper/SyncShortFieldWithEmptyMainFieldConcatenator.java
+++ implementation/scipamato/core/sync/src/main/java/ch/difty/scipamato/core/sync/jobs/paper/SyncShortFieldWithEmptyMainFieldConcatenator.java
@@ -30,7 +30,6 @@ import ch.difty.scipamato.core.db.tables.records.PaperRecord;
 class SyncShortFieldWithEmptyMainFieldConcatenator extends AbstractShortFieldConcatenator
     implements SyncShortFieldConcatenator {
 
-    private static final String RS         = "rs";
     private static final String UNABLE_MSG = "Unable to evaluate recordset";
 
     SyncShortFieldWithEmptyMainFieldConcatenator() {
