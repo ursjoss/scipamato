e4b202d442edaacf30b6903a7306241644ab7503
Urs Joss <urs.joss@gmx.ch>
2019-11-09 18:07:14 +0100

    Bump spring-boot-starter-parent to 2.2.1

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -9,7 +9,7 @@ object Lib {
     //region:dependencyVersions
     private const val kotlinVersion = "1.3.50"
 
-    private const val springBootVersion = "2.2.0.RELEASE"
+    private const val springBootVersion = "2.2.1.RELEASE"
     private const val springBootAdminVersion = "2.1.6"
     const val springCloudVersion = "Greenwich.SR3"
     private const val springCloudStarterVersion = "2.1.3.RELEASE"
