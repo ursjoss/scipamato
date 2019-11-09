e81eb213cf63ee09e38c46ec34d55532f09fc011
Urs Joss <urs.joss@gmx.ch>
2019-11-09 18:10:41 +0100

    Bump wicket-jquery-ui from 8.3.0 to 8.6.0

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -17,7 +17,7 @@ object Lib {
     private const val wicketSpringBootStarterVersion = "2.1.7"
     private const val wicketVersion = "8.6.1"
     private const val wicketstuffVersion = "8.6.0"
-    private const val wicketJqueryUiVersion = "8.3.0"
+    private const val wicketJqueryUiVersion = "8.6.0"
     private const val wicketBootstrapVersion = "2.0.11"
     private const val jasperReportVersion = "6.10.0"
     const val jooqVersion = "3.12.2"
