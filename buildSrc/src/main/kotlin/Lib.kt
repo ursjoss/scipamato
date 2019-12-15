ff48dbcb986d2adc5196ec35be248564a00b2785
Urs Joss <urs.joss@gmx.ch>
2019-12-15 23:24:44 +0100

    Bump jasperreports from 6.10.0 to 6.11.0

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -19,7 +19,7 @@ object Lib {
     private const val wicketstuffVersion = "8.6.0"
     private const val wicketJqueryUiVersion = "8.6.0"
     private const val wicketBootstrapVersion = "2.0.11"
-    private const val jasperReportVersion = "6.10.0"
+    private const val jasperReportVersion = "6.11.0"
     const val jooqVersion = "3.12.3"
 
     private const val kotlinLoggingVersion = "1.7.8"
