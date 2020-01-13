ba5c6501eae38b61df6190f4c09f95f99b0ddd23
Urs Joss <urs.joss@gmx.ch>
2020-01-13 20:56:23 +0100

    Bump lombok-plugin 4.1.6

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -58,7 +58,7 @@ object Lib {
 
     //region:pluginVersions
     private const val springDependencyManagementPluginVersion = "1.0.8.RELEASE"
-    private const val lombokPluginVersion = "4.1.5"
+    private const val lombokPluginVersion = "4.1.6"
     private const val jooqModelatorPluginVersion = "3.6.0"
     private const val reckonPluginVersion = "0.12.0"
     private const val jaxbPluginVersion = "4.0.0"
