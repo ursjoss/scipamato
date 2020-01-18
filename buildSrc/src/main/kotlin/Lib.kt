09b864b86249d416aeda51d3d228a3a25dc1ebe6
Urs Joss <urs.joss@gmx.ch>
2020-01-18 12:04:31 +0100

    Bump wicket-bootstrap from 2.0.11 to 2.0.12

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -18,7 +18,7 @@ object Lib {
     private const val wicketVersion = "8.7.0"
     private const val wicketstuffVersion = "8.7.0"
     private const val wicketJqueryUiVersion = "8.6.0"
-    private const val wicketBootstrapVersion = "2.0.11"
+    private const val wicketBootstrapVersion = "2.0.12"
     private const val jasperReportVersion = "6.11.0"
     const val jooqVersion = "3.12.3"
 
