6f17fb631f832870b81be335f242f6c96a8cfa34
Urs Joss <urs.joss@gmx.ch>
2020-01-22 22:08:21 +0100

    Bump junit from 5.5.2 to 5.6.0

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -40,7 +40,7 @@ object Lib {
 
     private const val equalsverifierVersion = "3.1.11"
 
-    private const val junit5Version = "5.5.2"
+    private const val junit5Version = "5.6.0"
 
     private const val testcontainersVersion = "1.12.4"
 
