9fa35d7e797e71dc0a37dbd72489ca69621eb744
Urs Joss <urs.joss@gmx.ch>
2020-02-29 20:29:44 +0100

    Bump spring-boot-starter from 2.2.4 to 2.2.5

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -9,7 +9,7 @@ object Lib {
     //region:dependencyVersions
     private const val kotlinVersion = "1.3.61"
 
-    private const val springBootVersion = "2.2.4.RELEASE"
+    private const val springBootVersion = "2.2.5.RELEASE"
     private const val springBootAdminVersion = "2.2.1"
     const val springCloudVersion = "Hoxton.SR1"
     private const val springCloudStarterVersion = "2.2.1.RELEASE"
