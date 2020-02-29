3875b47a4cc4b9f2dac22bf81c75f28add62983c
Urs Joss <urs.joss@gmx.ch>
2020-02-29 20:33:48 +0100

    Bump spring-cloud-starter from Hoxton.SR1 to SR2 and openfeign from 10.7.2 to 10.8

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -11,7 +11,7 @@ object Lib {
 
     private const val springBootVersion = "2.2.5.RELEASE"
     private const val springBootAdminVersion = "2.2.1"
-    const val springCloudVersion = "Hoxton.SR1"
+    const val springCloudVersion = "Hoxton.SR2"
     private const val springCloudStarterVersion = "2.2.1.RELEASE"
 
     private const val wicketSpringBootStarterVersion = "2.1.8"
@@ -24,7 +24,7 @@ object Lib {
 
     private const val kotlinLoggingVersion = "1.7.8"
 
-    private const val openfeignVersion = "10.7.2"
+    private const val openfeignVersion = "10.8"
 
     private const val jaxbCoreVersion = "2.3.0.1"
     private const val jaxbImplVersion = "2.3.2"
