e509265e89a4ae74400a24bf375d57c24ec250be
Urs Joss <urs.joss@gmx.ch>
2020-03-02 00:14:33 +0100

    Externalize copied JRis classes with KRis (#162)
    * Replace internal JRis classes with ch.difty.kris:kris-core
    * Clear core-web detekt baseline

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,6 +80,7 @@ allprojects {
         mavenCentral()
         jcenter()
         maven { url = uri("https://dl.bintray.com/mockito/maven/") }
+        maven { url = uri("https://dl.bintray.com/difty/maven/") }
         maven { url = uri("https://jaspersoft.jfrog.io/jaspersoft/third-party-ce-artifacts") }
     }
 }
