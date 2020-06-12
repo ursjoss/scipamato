ce81a6f2ca38be61bde7c23e3af78d0f08f35302
Urs Joss <urs.joss@gmx.ch>
2020-06-12 16:52:10 +0200

    Bump spring-boot-starter from 2.3.0 to 2.3.1

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -9,7 +9,7 @@ object Lib {
     //region:dependencyVersions
     private const val kotlinVersion = "1.3.72"
 
-    private const val springBootVersion = "2.3.0.RELEASE"
+    private const val springBootVersion = "2.3.1.RELEASE"
     private const val springBootAdminVersion = "2.2.2"
     const val springCloudVersion = "Hoxton.SR4"
     private const val springCloudStarterVersion = "2.2.2.RELEASE"
