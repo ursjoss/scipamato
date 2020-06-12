560c69df9fc92ce37a79aea18a5a227de4883ef4
Urs Joss <urs.joss@gmx.ch>
2020-06-12 17:23:18 +0200

    Bump sonarqube-gradle-plugin from 2.8 to 3.0

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -60,7 +60,7 @@ object Lib {
     private const val reckonPluginVersion = "0.12.0"
     private const val jaxbPluginVersion = "4.3.1"
     private const val testSetsPluginVersion = "3.0.1"
-    private const val sonarqubePluginVersion = "2.8"
+    private const val sonarqubePluginVersion = "3.0"
     private const val detektPluginVersion = "1.9.1"
     //endregion
 
