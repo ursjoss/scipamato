4ca3f316458273772150ea32a12f28a715678bc9
Urs Joss <urs.joss@gmx.ch>
2020-07-19 15:52:06 +0200

    Bump spring-mockk from 2.0.1 to 2.0.2

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -45,7 +45,7 @@ object Lib {
     private const val kwikVersion = "0.6.0"
     private const val kluentVersion = "1.61"
     private const val mockkVersion = "1.10.0"
-    private const val springMockkVersion = "2.0.1"
+    private const val springMockkVersion = "2.0.2"
 
     private const val jsr305Version = "3.0.2"
     //endregion
