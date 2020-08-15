abebada422df0d952a6cc3a4070d1a88be7f1b53
Urs Joss <urs.joss@gmx.ch>
2020-08-15 13:47:40 +0200

    Bump kotlin from 1.3.72 to 1.4.0

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -7,7 +7,7 @@ import java.util.Properties
 object Lib {
 
     //region:dependencyVersions
-    private const val kotlinVersion = "1.3.72"
+    private const val kotlinVersion = "1.4.0"
 
     private const val springBootVersion = "2.3.3.RELEASE"
     private const val springBootAdminVersion = "2.3.0"
