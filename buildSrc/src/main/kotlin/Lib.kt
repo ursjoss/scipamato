14ddbb902e08aa248bf7b27b86308eb22c48e153
Urs Joss <urs.joss@gmx.ch>
2020-10-01 15:16:26 +0200

    Bump wicket-jquery-ui from 9.0.0-M5.1 to 9.0.0

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -17,7 +17,7 @@ object Lib {
     private const val wicketSpringBootStarterVersion = "3.0.4"
     private const val wicketVersion = "9.0.0"
     private const val wicketstuffVersion = "9.0.0"
-    private const val wicketJqueryUiVersion = "9.0.0-M5.1"
+    private const val wicketJqueryUiVersion = "9.0.0"
     private const val wicketBootstrapVersion = "4.0.0"
     private const val jasperReportVersion = "6.14.0"
     private const val krisVersion = "0.2.5"
