d71402526cecba4564dee252b6b91179217d5c94
Urs Joss <urs.joss@gmx.ch>
2020-10-13 23:48:17 +0200

    Refactor PublicPaperFilterTest

--- public/public-entity/src/test/kotlin/ch/difty/scipamato/publ/entity/filter/PublicPaperFilterTest.kt
+++ public/public-entity/src/test/kotlin/ch/difty/scipamato/publ/entity/filter/PublicPaperFilterTest.kt
@@ -16,45 +16,46 @@ internal class PublicPaperFilterTest {
     @Suppress("MaximumLineLength", "MaxLineLength")
     @Test
     fun construct() {
-        val filter = PublicPaperFilter()
-        filter.number = 1L
-        filter.authorMask = "am"
-        filter.titleMask = "tm"
-        filter.methodsMask = "mm"
-        filter.publicationYearFrom = 2000
-        filter.publicationYearUntil = 3000
-        filter.populationCodes = listOf(PopulationCode.CHILDREN, PopulationCode.ADULTS)
-        filter.studyDesignCodes = listOf(StudyDesignCode.EXPERIMENTAL)
-        filter.codesOfClass1 = listOf(newCode("1A"), newCode("1B"))
-        filter.codesOfClass2 = listOf(newCode("2A"), newCode("2B"))
-        filter.codesOfClass3 = listOf(newCode("3A"), newCode("3B"))
-        filter.codesOfClass4 = listOf(newCode("4A"), newCode("4B"))
-        filter.codesOfClass5 = listOf(newCode("5A"), newCode("5B"))
-        filter.codesOfClass6 = listOf(newCode("6A"), newCode("6B"))
-        filter.codesOfClass7 = listOf(newCode("7A"), newCode("7B"))
-        filter.codesOfClass8 = listOf(newCode("8A"), newCode("8B"))
-        filter.keywords = listOf(Keyword(1, 1, "de", "k1", null))
+        val filter = PublicPaperFilter().apply {
+            number = 1L
+            authorMask = "am"
+            titleMask = "tm"
+            methodsMask = "mm"
+            publicationYearFrom = 2000
+            publicationYearUntil = 3000
+            populationCodes = listOf(PopulationCode.CHILDREN, PopulationCode.ADULTS)
+            studyDesignCodes = listOf(StudyDesignCode.EXPERIMENTAL)
+            codesOfClass1 = listOf(newCode("1A"), newCode("1B"))
+            codesOfClass2 = listOf(newCode("2A"), newCode("2B"))
+            codesOfClass3 = listOf(newCode("3A"), newCode("3B"))
+            codesOfClass4 = listOf(newCode("4A"), newCode("4B"))
+            codesOfClass5 = listOf(newCode("5A"), newCode("5B"))
+            codesOfClass6 = listOf(newCode("6A"), newCode("6B"))
+            codesOfClass7 = listOf(newCode("7A"), newCode("7B"))
+            codesOfClass8 = listOf(newCode("8A"), newCode("8B"))
+            keywords = listOf(Keyword(1, 1, "de", "k1", null))
 
-        filter.number shouldBeEqualTo 1L
-        filter.authorMask shouldBeEqualTo "am"
-        filter.titleMask shouldBeEqualTo "tm"
-        filter.methodsMask shouldBeEqualTo "mm"
-        filter.publicationYearFrom shouldBeEqualTo 2000
-        filter.publicationYearUntil shouldBeEqualTo 3000
+            number shouldBeEqualTo 1L
+            authorMask shouldBeEqualTo "am"
+            titleMask shouldBeEqualTo "tm"
+            methodsMask shouldBeEqualTo "mm"
+            publicationYearFrom shouldBeEqualTo 2000
+            publicationYearUntil shouldBeEqualTo 3000
 
-        filter.populationCodes shouldContainSame listOf(PopulationCode.CHILDREN, PopulationCode.ADULTS)
-        filter.studyDesignCodes shouldContainSame listOf(StudyDesignCode.EXPERIMENTAL)
+            populationCodes shouldContainSame listOf(PopulationCode.CHILDREN, PopulationCode.ADULTS)
+            studyDesignCodes shouldContainSame listOf(StudyDesignCode.EXPERIMENTAL)
 
-        filter.codesOfClass1 shouldHaveSize 2
-        filter.codesOfClass2 shouldHaveSize 2
-        filter.codesOfClass3 shouldHaveSize 2
-        filter.codesOfClass4 shouldHaveSize 2
-        filter.codesOfClass5 shouldHaveSize 2
-        filter.codesOfClass6 shouldHaveSize 2
-        filter.codesOfClass7 shouldHaveSize 2
-        filter.codesOfClass8 shouldHaveSize 2
+            codesOfClass1 shouldHaveSize 2
+            codesOfClass2 shouldHaveSize 2
+            codesOfClass3 shouldHaveSize 2
+            codesOfClass4 shouldHaveSize 2
+            codesOfClass5 shouldHaveSize 2
+            codesOfClass6 shouldHaveSize 2
+            codesOfClass7 shouldHaveSize 2
+            codesOfClass8 shouldHaveSize 2
 
-        filter.keywords.map { it.keywordId } shouldContainAll listOf(1)
+            keywords.map { it.keywordId } shouldContainAll listOf(1)
+        }
 
         filter.toString() shouldBeEqualTo
             "PublicPaperFilter(number=1, authorMask=am, titleMask=tm, methodsMask=mm, publicationYearFrom=2000, " +
@@ -78,13 +79,11 @@ internal class PublicPaperFilterTest {
             "keywords=[Keyword(id=1, keywordId=1, langCode=de, name=k1, searchOverride=null)])"
     }
 
-    private fun newCode(code: String): Code {
-        return Code.builder()
-            .code(code)
-            .codeClassId(Integer.parseInt(code.substring(0, 1)))
-            .langCode("en")
-            .build()
-    }
+    private fun newCode(code: String): Code = Code.builder()
+        .code(code)
+        .codeClassId(Integer.parseInt(code.substring(0, 1)))
+        .langCode("en")
+        .build()
 
     @Test
     fun equals() {
