592fccea47027332ba55164d442b09cc4b960d60
Urs Joss <urs.joss@gmx.ch>
2020-11-07 16:25:31 +0100

    Bump KRis from 0.2.6 to 0.3.0

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -20,7 +20,7 @@ object Lib {
     private const val wicketJqueryUiVersion = "9.1.0"
     private const val wicketBootstrapVersion = "4.0.1"
     private const val jasperReportVersion = "6.16.0"
-    private const val krisVersion = "0.2.6"
+    private const val krisVersion = "0.3.0"
     const val jooqVersion = "3.13.5"
     const val flywayVersion = "6.5.7"
 
