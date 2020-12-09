2f778bca3a7d69ace1ead96d214f9c95ce30674c
Urs Joss <urs.joss@gmx.ch>
2020-12-09 23:25:19 +0100

    Bump kotlin from 1.4.20 to 1.4.21

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -7,7 +7,7 @@ import java.util.Properties
 object Lib {
 
     //region:dependencyVersions
-    private const val kotlinVersion = "1.4.20"
+    private const val kotlinVersion = "1.4.21"
 
     private const val springBootVersion = "2.4.0"
     private const val springBootAdminVersion = "2.3.1"
