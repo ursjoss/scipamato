d204101bf3ad11d9de025c247c0bb19463dfe188
Urs Joss <urs.joss@gmx.ch>
2021-01-28 22:44:28 +0100

    Extract ReviewCsvBuilder from ResultPanel and test

---
+++ core/core-web/src/main/java/ch/difty/scipamato/core/web/paper/csv/CsvAdapter.kt
@@ -0,0 +1,98 @@
+package ch.difty.scipamato.core.web.paper.csv
+
+import ch.difty.scipamato.core.entity.Paper
+import ch.difty.scipamato.core.web.paper.jasper.ReportHeaderFields
+import ch.difty.scipamato.core.web.paper.jasper.review.PaperReview
+import com.univocity.parsers.csv.CsvFormat
+import com.univocity.parsers.csv.CsvWriter
+import com.univocity.parsers.csv.CsvWriterSettings
+import java.io.Serializable
+import java.io.StringWriter
+
+/**
+ * CSV Adapter accepting a list of objects of type [T]
+ * and builds a full CSV file as a single String.
+ *
+ * Works well for small number of records but doesn't scale well.
+ */
+interface CsvAdapter<T> : Serializable {
+    fun build(types: List<T>): String
+}
+
+@Suppress("FunctionName")
+abstract class AbstractCsvAdapter<T>(
+    private val rowMapper: (Collection<T>) -> List<Array<String>>,
+    private val headers: Array<String?>
+) : CsvAdapter<T> {
+
+    override fun build(types: List<T>): String {
+        val rows: List<Array<String>> = rowMapper(types)
+        StringWriterWithBom().apply {
+            CsvWriter(this, SemicolonDelimitedSettings()).apply {
+                writeHeaders(*headers)
+                writeRowsAndClose(rows)
+            }
+            return toString()
+        }
+    }
+
+    private fun StringWriterWithBom() = StringWriter().apply { write("\ufeff") }
+
+    private fun SemicolonDelimitedSettings() = CsvWriterSettings().apply {
+        format = CsvFormat().apply { delimiter = ';' }
+    }
+}
+
+/**
+ * Adapter to build the Review CSV file, accepting a list of [Paper]s
+ */
+class ReviewCsvAdapter(private val rhf: ReportHeaderFields) : AbstractCsvAdapter<Paper>(
+    rowMapper = { types -> types.map { PaperReview(it, rhf).toRow() } },
+    headers = arrayOf(
+        rhf.numberLabel,
+        rhf.authorYearLabel,
+        rhf.populationPlaceLabel,
+        rhf.methodOutcomeLabel,
+        rhf.exposurePollutantLabel,
+        rhf.methodStudyDesignLabel,
+        rhf.populationDurationLabel,
+        rhf.populationParticipantsLabel,
+        rhf.exposureAssessmentLabel,
+        rhf.resultExposureRangeLabel,
+        rhf.methodConfoundersLabel,
+        rhf.resultEffectEstimateLabel,
+        rhf.conclusionLabel,
+        rhf.commentLabel,
+        rhf.internLabel,
+        rhf.goalsLabel,
+        rhf.populationLabel,
+        rhf.methodsLabel,
+        rhf.resultLabel,
+    ),
+) {
+    companion object {
+        const val FILE_NAME = "review.csv"
+    }
+}
+
+private fun PaperReview.toRow() = arrayOf(
+    number,
+    authorYear,
+    populationPlace,
+    methodOutcome,
+    exposurePollutant,
+    methodStudyDesign,
+    populationDuration,
+    populationParticipants,
+    exposureAssessment,
+    resultExposureRange,
+    methodConfounders,
+    resultEffectEstimate,
+    conclusion,
+    comment,
+    intern,
+    goals,
+    population,
+    methods,
+    result,
+)
---
+++ core/core-web/src/test/kotlin/ch/difty/scipamato/core/web/paper/csv/ReviewCsvBuilderSpek.kt
@@ -0,0 +1,79 @@
+package ch.difty.scipamato.core.web.paper.csv
+
+import ch.difty.scipamato.core.entity.Paper
+import ch.difty.scipamato.core.web.paper.jasper.ReportHeaderFields
+import org.amshove.kluent.shouldBeEqualTo
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+private const val BOM = "\ufeff"
+
+object ReviewCsvBuilderSpek : Spek({
+
+    describe("csv builder") {
+        val builder = ReviewCsvAdapter(            rhf = rhf        )
+        it("can build CSV with multiline fields with dynamic quoting") {
+            builder.build(listOf(PaperWithIndex(1), PaperWithIndex(2))) shouldBeEqualTo """
+                ${BOM}nl;ayl;ppl;mol;epl;msdl;pdl;ppl;eal;rerl;mcl;reel;cl1;cl2;il;gl;pl;ml;rl
+                1;Bond1 J 2021;pp1;mo1;eo1;msd1;pd1;pp1;ea1;rer1;mc1;ree1;c11;c21;i1;g1;p1;m1;r1
+                2;Bond2 J 2022;pp2;mo2;eo2;msd2;pd2;pp2;ea2;rer2;mc2;ree2;c12;"c22
+                foo
+                bar
+                baz";i2;g2;p2;m2;r2
+
+                """.trimIndent()
+        }
+    }
+})
+
+private val rhf = ReportHeaderFields(
+    headerPart = "hp",
+    brand = "b",
+    numberLabel = "nl",
+    authorYearLabel = "ayl",
+    populationPlaceLabel = "ppl",
+    methodOutcomeLabel = "mol",
+    exposurePollutantLabel = "epl",
+    methodStudyDesignLabel = "msdl",
+    populationDurationLabel = "pdl",
+    populationParticipantsLabel = "ppl",
+    exposureAssessmentLabel = "eal",
+    resultExposureRangeLabel = "rerl",
+    methodConfoundersLabel = "mcl",
+    resultEffectEstimateLabel = "reel",
+    conclusionLabel = "cl1",
+    commentLabel = "cl2",
+    internLabel = "il",
+    goalsLabel = "gl",
+    populationLabel = "pl",
+    methodsLabel = "ml",
+    resultLabel = "rl",
+)
+
+@Suppress("TestFunctionName")
+private fun PaperWithIndex(i: Int) = Paper().apply {
+    number = i.toLong()
+    firstAuthor = "Bond$i J"
+    publicationYear = 2020 + i
+    populationPlace = "pp$i"
+    methodOutcome = "mo$i"
+    exposurePollutant = "eo$i"
+    methodStudyDesign = "msd$i"
+    populationDuration = "pd$i"
+    populationParticipants = "pp$i"
+    exposureAssessment = "ea$i"
+    resultExposureRange = "rer$i"
+    methodConfounders = "mc$i"
+    resultEffectEstimate = "ree$i"
+    conclusion = "c1$i"
+    comment = if (i == 1) "c2$i" else """c2$i
+        |foo
+        |bar
+        |baz
+    """.trimMargin()
+    intern = "i$i"
+    goals = "g$i"
+    population = "p$i"
+    methods = "m$i"
+    result = "r$i"
+}
