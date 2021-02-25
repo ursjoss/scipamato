e9d224d3b36fe5d4edd64c08fd904a9f41bf6897
Urs Joss <urs.joss@gmx.ch>
2021-02-25 21:19:10 +0100

    Bump equalsverifier from 3.5.4 to 3.5.5

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -39,7 +39,7 @@ object Lib {
 
     private const val fontAwesomeVersion = "5.15.0"
 
-    private const val equalsverifierVersion = "3.5.4"
+    private const val equalsverifierVersion = "3.5.5"
 
     private const val junit5Version = "5.7.1"
 
