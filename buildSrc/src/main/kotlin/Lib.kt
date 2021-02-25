1b4887eeb4bd1a2c1e92ee6d776ddae1c6d69fac
Urs Joss <urs.joss@gmx.ch>
2021-02-25 21:18:41 +0100

    Bump spring-boot-admin from 2.3.1 to 2.4.0

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -10,7 +10,7 @@ object Lib {
     private const val kotlinVersion = "1.4.31"
 
     private const val springBootVersion = "2.4.3"
-    private const val springBootAdminVersion = "2.3.1"
+    private const val springBootAdminVersion = "2.4.0"
     const val springCloudVersion = "2020.0.0"
     private const val springCloudStarterVersion = "3.0.0"
 
