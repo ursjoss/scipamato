0f31c2ace2162f08c7f86222e3c7101df9365b43
Urs Joss <urs.joss@gmx.ch>
2021-02-27 17:07:47 +0100

    Add a few tests regarding enableed-state of the delete button
     * Unfortunately I can't capture the current failure - the confirmation beheavior is not kicking in :-(

--- core/core-web/src/test/kotlin/ch/difty/scipamato/core/web/paper/search/SearchOrderSelectorPanelInEditModeTest.kt
+++ core/core-web/src/test/kotlin/ch/difty/scipamato/core/web/paper/search/SearchOrderSelectorPanelInEditModeTest.kt
@@ -2,18 +2,13 @@ package ch.difty.scipamato.core.web.paper.search
 
 import ch.difty.scipamato.common.web.Mode
 import io.mockk.verify
+import org.apache.wicket.model.Model
 import org.junit.jupiter.api.Test
 
 internal class SearchOrderSelectorPanelInEditModeTest : SearchOrderSelectorPanelTest() {
 
     override val mode: Mode
         get() = Mode.EDIT
-
-    @Test
-    fun loadingPage_withSearchOrderWithCurrentOwner_rendersGlobalCheckBoxDisabled() {
-        tester.startComponentInPage(makePanel())
-        tester.assertEnabled("$PANEL_ID:form:global")
-    }
 
     @Test
     fun withGlobalSearchOrders_withSameOwner_globalCheckBox_enabled() {
@@ -45,5 +40,47 @@ internal class SearchOrderSelectorPanelInEditModeTest : SearchOrderSelectorPanel
         searchOrder.apply { owner = OWNER_ID + 1 }
         tester.startComponentInPage(makePanel())
         tester.assertDisabled("$PANEL_ID:form:global")
+    }
+
+    @Test
+    fun deleteButton_withNoSearchSelected_isDisabled() {
+        tester.startComponentInPage(SearchOrderSelectorPanel(PANEL_ID, Model.of(), mode))
+        tester.assertDisabled("$PANEL_ID:form:delete")
+    }
+
+    @Test
+    fun deleteButton_withGlobalSearchOfOtherUserSelected_isDisabled() {
+        searchOrder.apply {
+            owner = OWNER_ID + 1
+            isGlobal = true
+        }
+        tester.startComponentInPage(makePanel())
+        tester.assertDisabled("$PANEL_ID:form:delete")
+    }
+
+
+    @Test
+    fun deleteButton_withGlobalSearchOfCurrentUserSelected_isEnabled() {
+        searchOrder.apply {
+            isGlobal = true
+        }
+        tester.startComponentInPage(makePanel())
+        tester.assertEnabled("$PANEL_ID:form:delete")
+    }
+
+    @Test
+    fun deleteButton_withLocalSearchOfCurrentUserSelected_isEnabled() {
+        tester.startComponentInPage(makePanel())
+        tester.assertEnabled("$PANEL_ID:form:delete")
+        verify(exactly = 0) { searchOrderServiceMock.remove(any()) }
+    }
+
+    @Test
+    fun deleting_doesDelete() {
+        // Why isn't it respecting the ConfirmationBehavior in the test setting?
+        tester.startComponentInPage(makePanel())
+        tester.clickLink("$PANEL_ID:form:delete")
+        verify(exactly = 1) { searchOrderServiceMock.remove(any()) }
+        tester.assertRenderedPage(PaperSearchPage::class.java)
     }
 }
