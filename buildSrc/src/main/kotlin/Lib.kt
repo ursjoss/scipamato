d724a644faee590620f34f3034fff5e5ea5a8b8f
Urs Joss <urs.joss@gmx.ch>
2021-03-20 17:03:36 +0100

    Bump spring-boot from 2.4.3 to 2.4.4

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -9,7 +9,7 @@ object Lib {
     //region:dependencyVersions
     private const val kotlinVersion = "1.4.31"
 
-    private const val springBootVersion = "2.4.3"
+    private const val springBootVersion = "2.4.4"
     private const val springBootAdminVersion = "2.4.0"
     const val springCloudVersion = "2020.0.0"
     private const val springCloudStarterVersion = "3.0.0"
