874c024822375e75712451574c76b738fe37f7c5
Urs Joss <urs.joss@gmx.ch>
2021-03-28 16:29:44 +0200

    Bump KRis from 0.3.1 to 0.3.2

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -22,7 +22,7 @@ object Lib {
     private const val jasperReportVersion = "6.16.0"
     private const val univocityParsersVersion = "2.9.1"
 
-    private const val krisVersion = "0.3.1"
+    private const val krisVersion = "0.3.2"
     const val jooqVersion = "3.14.8"
     const val flywayVersion = "7.7.0"
 
