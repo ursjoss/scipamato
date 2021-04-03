2028287691b527833633e1bc7bdcd1769c8691b8
Urs Joss <urs.joss@gmx.ch>
2021-04-03 13:43:51 +0200

    Bump wicket and friends
     * wicket from 9.2.0 to 9.3.0
     * wicketstuff from 9.2.0 to 9.3.0
     * wicket-jquery-ui from 9.2.1 to 9.3.1
     * wicket-bootstrap from 4.0.3 to 4.0.4

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -15,10 +15,10 @@ object Lib {
     private const val springCloudStarterVersion = "3.0.2"
 
     private const val wicketSpringBootStarterVersion = "3.0.4"
-    private const val wicketVersion = "9.2.0"
-    private const val wicketstuffVersion = "9.2.0"
-    private const val wicketJqueryUiVersion = "9.2.1"
-    private const val wicketBootstrapVersion = "4.0.3"
+    private const val wicketVersion = "9.3.0"
+    private const val wicketstuffVersion = "9.3.0"
+    private const val wicketJqueryUiVersion = "9.3.1"
+    private const val wicketBootstrapVersion = "4.0.4"
     private const val jasperReportVersion = "6.16.0"
     private const val univocityParsersVersion = "2.9.1"
 
