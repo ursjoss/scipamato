5f358d147044f44f0feb35ebd23017c8170e4826
Urs Joss <urs.joss@gmx.ch>
2021-04-04 13:30:53 +0200

    Bump jaxb-gradle-plugin from 4.4.1 to 4.4.2

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -60,7 +60,7 @@ object Lib {
     private const val jooqModelatorPluginVersion = "3.9.0"
     private const val reckonPluginVersion = "0.13.0"
     private const val versionsPluginVersion = "0.38.0"
-    private const val jaxbPluginVersion = "4.4.1"
+    private const val jaxbPluginVersion = "4.4.2"
     private const val testSetsPluginVersion = "3.0.1"
     private const val sonarqubePluginVersion = "3.1.1"
     private const val detektPluginVersion = "1.16.0"
