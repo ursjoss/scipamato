a684b3cbc665e6eae51e4b2cc7e9d15709e7bff1
Urs Joss <urs.joss@gmx.ch>
2021-05-24 18:35:51 +0200

    Bump kotlin from 1.4.32 to 1.5.10

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -7,7 +7,7 @@ import java.util.Properties
 object Lib {
 
     //region:dependencyVersions
-    private const val kotlinVersion = "1.4.32"
+    private const val kotlinVersion = "1.5.10"
 
     private const val springBootVersion = "2.5.0"
     private const val springBootAdminVersion = "2.4.1"
