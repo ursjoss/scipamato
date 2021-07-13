9e6be7422f0df2957402b11b1742d64e85fa0bb6
Urs Joss <urs.joss@gmx.ch>
2021-07-13 20:11:40 +0200

    Bump flyway from 7.11.1 to 7.11.2

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -24,7 +24,7 @@ object Lib {
 
     private const val krisVersion = "0.3.2"
     const val jooqVersion = "3.14.12"
-    const val flywayVersion = "7.11.1"
+    const val flywayVersion = "7.11.2"
 
     private const val kotlinLoggingVersion = "2.0.8"
 
