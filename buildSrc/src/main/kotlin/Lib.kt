9b8d698b17b71518c0eb4b6c4286860feca1aec2
Urs Joss <urs.joss@gmx.ch>
2021-08-22 12:37:21 +0200

    Bump openfeign from 11.2 to 11.6

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -28,7 +28,7 @@ object Lib {
 
     private const val kotlinLoggingVersion = "2.0.10"
 
-    private const val openfeignVersion = "11.2"
+    private const val openfeignVersion = "11.6"
 
     private const val jaxbCoreVersion = "3.0.2"
     private const val jaxbImplVersion = "3.0.2"
