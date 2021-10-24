d6f3aede69faff40e39dde9185bcfadc68f4996b
Urs Joss <urs.joss@gmx.ch>
2021-10-24 21:02:42 +0200

    Bump spring-boot-starter-admin from 2.5.1 to 2.5.2

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -10,7 +10,7 @@ object Lib {
     private const val kotlinVersion = "1.5.31"
 
     private const val springBootVersion = "2.5.6"
-    private const val springBootAdminVersion = "2.5.1"
+    private const val springBootAdminVersion = "2.5.2"
     const val springCloudVersion = "2020.0.3"
     private const val springCloudStarterVersion = "3.0.3"
 
