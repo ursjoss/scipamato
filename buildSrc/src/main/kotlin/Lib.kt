9620e0d4acdfa4d62a2426857370ea963d120491
Urs Joss <urs.joss@gmx.ch>
2021-12-15 22:07:32 +0100

    chore(deps): Bump versions of dependencies (#314)
    * Bump log4j from 2.15.0 to 2.16.0
    * Bump flyway from 8.2.1 to 8.2.2
    * Bump equalsverifier from 3.8.1 to 3.8.2
    * Bump gradle wrapper from 7.3.1 to 7.3.2

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -24,7 +24,7 @@ object Lib {
 
     private const val krisVersion = "0.3.2"
     const val jooqVersion = "3.15.5"
-    const val flywayVersion = "8.2.1"
+    const val flywayVersion = "8.2.2"
 
     private const val kotlinLoggingVersion = "2.1.16"
 
@@ -33,7 +33,7 @@ object Lib {
     private const val javaxElApiVersion = "3.0.1-b06"
     private const val javaxElVersion = "3.0.1-b12"
 
-    private const val equalsverifierVersion = "3.8"
+    private const val equalsverifierVersion = "3.8.1"
 
     private const val junit5Version = "5.8.2"
 
