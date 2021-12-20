de00fcbc4a5073d33fc884b583deb0c2829523ef
Urs Joss <urs.joss@gmx.ch>
2021-12-20 07:19:22 +0100

    chore(deps): Bump logback from 1.2.7 to 1.2.9
    Mitigation for [CVE-2021-42550]

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ extra["spring.cloudVersion"] = Lib.springCloudVersion
 extra["jooq.version"] = Lib.jooqVersion
 extra["flyway.version"] = Lib.flywayVersion
 // temporary workaround
+extra["logback.version"] = "1.2.9"
 extra["log4j2.version"] = "2.17.0"
 
 dependencyManagement {
