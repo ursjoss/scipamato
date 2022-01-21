319db1a7f6f45a04707ad3660e1b434d4a257914
Urs Joss <urs.joss@gmx.ch>
2022-01-21 05:56:09 +0100

    Bump flyway from 0.4.0 to 0.4.2

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -25,7 +25,7 @@ object Lib {
 
     private const val krisVersion = "0.3.3"
     const val jooqVersion = "3.15.5"
-    const val flywayVersion = "8.4.0"
+    const val flywayVersion = "8.4.2"
 
     private const val kotlinLoggingVersion = "2.1.21"
 
