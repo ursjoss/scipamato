28da0e951ccb4f793f6dfd016e330dabc208b07a
Urs Joss <urs.joss@gmx.ch>
2022-02-25 11:17:27 +0100

    Bump jOOQ from 3.15.6 to 3.15.8

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -24,7 +24,7 @@ object Lib {
     private const val univocityParsersVersion = "2.9.1"
 
     private const val krisVersion = "0.3.3"
-    const val jooqVersion = "3.15.6"
+    const val jooqVersion = "3.15.8"
     const val flywayVersion = "8.5.1"
 
     private const val kotlinLoggingVersion = "2.1.21"
