ec631f574f49734b792f49635a23c4cdbc2247d1
Urs Joss <urs.joss@gmx.ch>
2022-02-25 12:25:06 +0100

    chore(deps): Bump versions of dependncies (#321)
    * Bump spring-boot from 2.6.3 to 2.6.4
    * Bump springmockk from 3.1.0 to 3.1.1
    * Bump versions gradle plugin from 0.41.0 to 0.42.0
    * Bump wicket-jquery-ui from 9.7.0 to 9.8.0
    * Bump spring-boot-admin from 2.6.1 to 2.6.2
    * Bump lombok-gradle-plugin from 6.3.0 to 6.4.1
    * Bump jasperreports from 6.18.1 to 6.19.0
    * Bump equalsverifier from 3.8.2 to 3.9
    * Bump reckon-gradle-plugin from 0.13.1 to 0.16.0
    * Bump wicket from 9.7.0 to 9.8.0
    * Bump flyway from 0.4.2 to 8.5.1
    * Bump jOOQ from 3.15.6 to 3.15.8
    * Bump wicketstuff from 9.7.0 to 9.8.0
    * Bump gradle-wrapper from 7.3.3 to 7.4

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -10,22 +10,22 @@ object Lib {
     private const val kotlinVersion = "1.6.10"
     private const val coroutinesVersion = "1.6.0"
 
-    private const val springBootVersion = "2.6.3"
-    private const val springBootAdminVersion = "2.6.1"
+    private const val springBootVersion = "2.6.4"
+    private const val springBootAdminVersion = "2.6.2"
     const val springCloudVersion = "2021.0.0"
     private const val springCloudStarterVersion = "3.1.0"
 
     private const val wicketSpringBootStarterVersion = "3.1.6"
-    private const val wicketVersion = "9.7.0"
-    private const val wicketstuffVersion = "9.7.0"
-    private const val wicketJqueryUiVersion = "9.7.0"
+    private const val wicketVersion = "9.8.0"
+    private const val wicketstuffVersion = "9.8.0"
+    private const val wicketJqueryUiVersion = "9.8.0"
     private const val wicketBootstrapVersion = "4.0.4"
-    private const val jasperReportVersion = "6.18.1"
+    private const val jasperReportVersion = "6.19.0"
     private const val univocityParsersVersion = "2.9.1"
 
     private const val krisVersion = "0.3.3"
-    const val jooqVersion = "3.15.6"
-    const val flywayVersion = "8.4.2"
+    const val jooqVersion = "3.15.8"
+    const val flywayVersion = "8.5.1"
 
     private const val kotlinLoggingVersion = "2.1.21"
 
@@ -34,7 +34,7 @@ object Lib {
     private const val javaxElApiVersion = "3.0.1-b06"
     private const val javaxElVersion = "3.0.1-b12"
 
-    private const val equalsverifierVersion = "3.8.2"
+    private const val equalsverifierVersion = "3.9"
 
     private const val junit5Version = "5.8.2"
 
@@ -44,7 +44,7 @@ object Lib {
     private const val kwikVersion = "0.8.4"
     private const val kluentVersion = "1.68"
     private const val mockkVersion = "1.12.2"
-    private const val springMockkVersion = "3.1.0"
+    private const val springMockkVersion = "3.1.1"
     const val jacocoToolVersion = "0.8.7"
 
     private const val jsr305Version = "3.0.2"
@@ -52,10 +52,10 @@ object Lib {
 
     //region:pluginVersions
     private const val springDependencyManagementPluginVersion = "1.0.11.RELEASE"
-    private const val lombokPluginVersion = "6.3.0"
+    private const val lombokPluginVersion = "6.4.1"
     private const val jooqModelatorPluginVersion = "3.9.0"
-    private const val reckonPluginVersion = "0.13.1"
-    private const val versionsPluginVersion = "0.41.0"
+    private const val reckonPluginVersion = "0.16.1"
+    private const val versionsPluginVersion = "0.42.0"
     private const val jaxbPluginVersion = "5.1.0"
     private const val testSetsPluginVersion = "4.0.0"
     private const val sonarqubePluginVersion = "3.3"
