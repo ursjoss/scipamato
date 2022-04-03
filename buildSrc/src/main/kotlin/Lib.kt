0d2435461b47687e97fea95ecfd4219b6085ec13
Urs Joss <urs.joss@gmx.ch>
2022-04-03 10:57:18 +0200

    Bump kotlin from 1.6.10 to 1.6.20

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -7,7 +7,7 @@ import java.util.Properties
 object Lib {
 
     //region:dependencyVersions
-    private const val kotlinVersion = "1.6.10"
+    private const val kotlinVersion = "1.6.20"
     private const val coroutinesVersion = "1.6.0"
 
     private const val springBootVersion = "2.6.6"
