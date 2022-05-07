2bcf97db3c41be0ea3a6a44e0bc4244cfdce90d6
Urs Joss <urs.joss@gmx.ch>
2022-05-07 15:19:03 +0200

    Bump flyway from 8.5.8 to 8.5.10

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -25,7 +25,7 @@ object Lib {
 
     private const val krisVersion = "0.3.3"
     const val jooqVersion = "3.15.10"
-    const val flywayVersion = "8.5.8"
+    const val flywayVersion = "8.5.10"
 
     private const val kotlinLoggingVersion = "2.1.21"
 
