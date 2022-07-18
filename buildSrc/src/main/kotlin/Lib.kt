426ff1d7b0b0a9ba63c5702fd09aeca6b549f0b0
Urs Joss <urs.joss@gmx.ch>
2022-07-18 19:08:33 +0200

    Bump wicket and wicketstuff from 9.10.0 to 9.11.0

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -16,8 +16,8 @@ object Lib {
     private const val springCloudStarterVersion = "3.1.3"
 
     private const val wicketSpringBootStarterVersion = "3.1.6"
-    private const val wicketVersion = "9.10.0"
-    private const val wicketstuffVersion = "9.10.0"
+    private const val wicketVersion = "9.11.0"
+    private const val wicketstuffVersion = "9.11.0"
     private const val wicketJqueryUiVersion = "9.8.1"
     private const val wicketBootstrapVersion = "4.0.4"
     private const val jasperReportVersion = "6.20.0"
