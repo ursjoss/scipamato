79f0843cf7e5fcf11e34cd0d005794ea33856d73
Urs Joss <urs.joss@gmx.ch>
2023-12-07 21:00:08 +0100

    fix test to run with dslContext

--- core/core-web/src/test/kotlin/ch/difty/scipamato/core/web/AbstractWicketTest.kt
+++ core/core-web/src/test/kotlin/ch/difty/scipamato/core/web/AbstractWicketTest.kt
@@ -22,9 +22,11 @@ import ch.difty.scipamato.core.persistence.UserService
 import ch.difty.scipamato.core.pubmed.PubmedArticleService
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.junit5.MockKExtension
+import org.jooq.DSLContext
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.test.context.ActiveProfiles
@@ -85,4 +87,7 @@ abstract class AbstractWicketTest {
 
     @MockkBean
     protected lateinit var userServiceMock: UserService
+
+    @MockkBean(relaxed = true)
+    protected lateinit var dsl: DSLContext
 }
