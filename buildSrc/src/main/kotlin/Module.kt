cdf0e5d559c5fe643f1564f8760c1d83f0e9dd02
Urs Joss <urs.joss@gmx.ch>
2025-11-24 22:06:21 +0100

    refactor: remove buildSrc

--- buildSrc/src/main/kotlin/Module.kt
+++
@@ -1,8 +0,0 @@
-object Module {
-
-    fun scipamatoCommonProjects(modules: List<String>) = modules.map { "common-$it" }.toTypedArray()
-
-    fun scipamatoCoreProjects(modules: List<String>) = modules.map { "core-$it" }.toTypedArray()
-
-    fun scipamatoPublicProjects(modules: List<String>) = modules.map { "public-$it" }.toTypedArray()
-}
