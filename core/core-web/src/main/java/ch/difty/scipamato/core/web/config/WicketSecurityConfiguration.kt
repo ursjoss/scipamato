10248fbb293c225acadfcd7cd00306c1946ac94b
Urs Joss <urs.joss@gmx.ch>
2026-05-27 22:50:37 +0200

    fix imports for EndpointRequest

--- core/core-web/src/main/java/ch/difty/scipamato/core/web/config/WicketSecurityConfiguration.kt
+++ core/core-web/src/main/java/ch/difty/scipamato/core/web/config/WicketSecurityConfiguration.kt
@@ -1,6 +1,6 @@
 package ch.difty.scipamato.core.web.config
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest
+import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
